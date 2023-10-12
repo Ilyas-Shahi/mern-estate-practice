1e33d306ce18e685ac2fa3c4e4cfabbb7b1449f3
Ilyas-Shahi <ilyasm.shahi359@gmail.com>
2023-10-13 00:03:54 +0500

    backend setup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import userRouter from './routes/user.route.js';
+
+dotenv.config();
 
 const app = express();
+
+app.use('/api/user', userRouter);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
