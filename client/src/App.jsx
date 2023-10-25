4f5a4dd6f2831a414b3d394119862351c3be94d6
Ilyas-Shahi <ilyasm.shahi359@gmail.com>
2023-10-26 00:14:41 +0500

    listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
 
 export default function App() {
   const { currentUser } = useSelector((state) => state.user);
@@ -20,6 +22,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
 
         <Route
           path="/profile"
@@ -28,6 +31,10 @@ export default function App() {
         <Route
           path="/create-listing"
           element={currentUser ? <CreateListing /> : <Navigate to="/sign-in" />}
+        />
+        <Route
+          path="/update-listing/:listingId"
+          element={currentUser ? <UpdateListing /> : <Navigate to="/sign-in" />}
         />
       </Routes>
     </BrowserRouter>
